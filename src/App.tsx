d9a1d946d40a8c350177384354342418a7271668
southliu <1275093225@qq.com>
2023-03-06 17:06:01 +0800

    feat: init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Game from './game/index'
+
+function App() {
+  return (
+    <>
+      <Game />
+    </>
+  )
+}
+
+export default App
---
+++ src/game/index.tsx
@@ -0,0 +1,13 @@
+import { Canvas } from '@react-three/fiber'
+
+function Game() {
+  return (
+    <div>
+      <Canvas>
+
+      </Canvas>
+    </div>
+  )
+}
+
+export default Game
